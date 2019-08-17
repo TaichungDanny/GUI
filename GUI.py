3893280769f1a42f64685e68c40e6f63fe1fc4b6
Danny Lin <d033016@gmail.com>
2019-08-17 11:50:17 +0800

    add button

--- GUI.py
+++ GUI.py
@@ -1,6 +1,12 @@
 import tkinter as tk
+def click1():
+    textvar.set('我被按過了')
 Danny = tk.Tk()
 Danny.title('Danny')
 label1 = tk.Label(Danny, text='標籤', fg='blue', bg='orange', font=('新細明體', 12), padx=20, pady=10)
 label1.pack()
+textvar = tk.StringVar()
+button1 = tk.Button(Danny, textvariable=textvar, command=click1)
+textvar.set('按鈕')
+button1.pack()
 Danny.mainloop()
