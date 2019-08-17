16fb154f35da684409343405e35ca6b30c94309c
Danny Lin <d033016@gmail.com>
2019-08-17 12:16:20 +0800

    add change text content function

--- GUI.py
+++ GUI.py
@@ -1,12 +1,22 @@
 import tkinter as tk
 def click1():
-    textvar.set('我被按過了')
+    global count
+    count += 1
+    labeltext.set('你按我' + str(count) + '次了')
+    textvar.set('按我')
+
+
+
 Danny = tk.Tk()
+labeltext = tk.StringVar()
 Danny.title('Danny')
-label1 = tk.Label(Danny, text='標籤', fg='blue', bg='orange', font=('新細明體', 12), padx=20, pady=10)
+Danny.geometry('450x150')
+labeltext.set('歡迎光臨')
+count = 0
+label1 = tk.Label(Danny, fg='blue', font=('新細明體', 12), padx=20, pady=10, textvariable=labeltext)
 label1.pack()
 textvar = tk.StringVar()
 button1 = tk.Button(Danny, textvariable=textvar, command=click1)
-textvar.set('按鈕')
+textvar.set('按我')
 button1.pack()
 Danny.mainloop()
